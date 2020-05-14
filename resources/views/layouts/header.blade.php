499727eff69d02e3fe97eb752269807550374f13
JeyM1 <denafen2000@gmail.com>
2020-05-14 22:04:11 +0300

    header: static version

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg header">
+<!--<nav class="navbar navbar-expand-lg header">
     <div class="container-fluid align-items-center">
         <a class="navbar-brand logo" href="#">Gutsul Games</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,7 +30,7 @@
                     <a class="button_main" id="register" href="#">Реєстрація</a>
                 </div>
                 
-            </div>
+            </div>-->
             <!--<div class="header_links mr-auto justify-content-between">
                 <div class="nav-item">
                     <a class="nav-link" href="#">Головна</a>
@@ -45,7 +45,51 @@
             
             <div class="d-flex col-sm-4 justify-content-between">
                 
-            </div>-->
+            </div>
+        </div>
+    </div>
+</nav>-->
+
+
+<nav class="navbar navbar-expand-lg header">
+    <div class="container-fluid justify-content-center">
+        <div class="row col-md-10 col-xs-12 padding_none">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="">
+                <a class="navbar-brand logo" href="#">Gutsul Games</a>
+            </div>
+            <div class="col-sm-8 col-xs-2 collapse navbar-collapse padding_none" id="navbarSupportedContent">
+                <div class="col-12 d-flex align-items-center padding_none">
+                    <div class="d-none d-xl-block">
+                        <div class="nav-item d-inline-flex">
+                            <a class="nav-link font_17" href="#">Головна</a>
+                        </div>
+                        <div class="nav-item d-inline-flex">
+                            <a class="nav-link font_17" href="#">Каталог</a>
+                        </div>
+                        <div class="nav-item d-inline-flex">
+                            <a class="nav-link font_17" href="#">Про нас</a>
+                        </div>
+                    </div>
+                    <div class="nav-icons d-inline-flex">
+                        <a href="#">
+                            <img src="./images/shopping-cart.svg">
+                        </a>
+                        <a href="#">
+                            <img src="./images/search.svg">
+                        </a>
+                    </div>
+                    <div class="spacer"></div>
+                    <div style="margin-right: 10px" class="d-inline-flex align-self-end">
+                        <a class="button_main font_17" id="login" href="#">Логін</a>
+                    </div>
+                    <div class="d-inline-flex align-self-end">
+                        <a class="button_main font_17" id="register" href="#">Реєстрація</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </nav>
