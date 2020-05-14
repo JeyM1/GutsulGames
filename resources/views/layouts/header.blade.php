27d5186a69c0f6595daed16558437113f9537467
ZaykaNya <48089558+ZaykaNya@users.noreply.github.com>
2020-05-14 17:09:12 +0300

    catalog

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,21 +1,39 @@
 <nav class="navbar navbar-expand-lg header">
-    <div class="container align-items-center">
+    <div class="container-fluid align-items-center">
         <a class="navbar-brand logo" href="#">Gutsul Games</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto justify-content-between">
-                <li class="nav-item">
+            <div class="col-12 d-flex">
+                <div class="nav-item d-inline-flex">
                     <a class="nav-link" href="#">Головна</a>
-                </li>
-                <li class="nav-item">
+                </div>
+                <div class="nav-item d-inline-flex">
                     <a class="nav-link" href="#">Каталог</a>
-                </li>
-                <li class="nav-item">
+                </div>
+                <div class="nav-item d-inline-flex">
                     <a class="nav-link" href="#">Про нас</a>
-                </li>
-            </ul>
+                </div>
+                <div class="d-inline-flex  align-self-end">
+                    <a class="button_main" id="login" href="#">Логін</a>
+                </div>
+                <div class="d-inline-flex  align-self-end">
+                    <a class="button_main" id="register" href="#">Реєстрація</a>
+                </div>
+                
+            </div>
+            <!--<div class="header_links mr-auto justify-content-between">
+                <div class="nav-item">
+                    <a class="nav-link" href="#">Головна</a>
+                </div>
+                <div class="nav-item">
+                    <a class="nav-link" href="#">Каталог</a>
+                </div>
+                <div class="nav-item">
+                    <a class="nav-link" href="#">Про нас</a>
+                </div>
+            </div>
             <div>
                 <a href="#">
                     <img src="./images/shopping-cart.svg">
@@ -25,9 +43,8 @@
                 </a>
             </div>
             <div class="d-flex col-sm-4 justify-content-between">
-                <a class="button_main" id="login" href="#">Логін</a>
-                <a class="button_main" id="register" href="#">Реєстрація</a>
-            </div>
+                
+            </div>-->
         </div>
     </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,66 +2,18 @@
 
 
 @section('content')
-    <div id="main_page">
-        <div>
-            <a class="button_main" href="">Зареєструватися</a>
-        </div>
-    </div>
     <div class="container-fluid">
-        <div class="row bg_dark justify-content-center">
-            <div class="content_block col-md-10 col-xs-12">
-                <p class="main_text">
-                    <span class="text_purple">Gutsul Games</span> - це цифрова вітрина з добірними іграми для ПК і Mac, 
-                    розроблена для зручності як гравців, так і творців контенту. 
-                </p>
-                <p class="main_text">
-                    <span class="text_purple">Наше головне завдання</span> - надавати відмінні ігри гравцям і прекрасні умови 
-                    для розробників. 
-                </p>
-                <div class="d-flex justify-content-center">
-                    <a class="button_main button_filled font_25" href="">Детальніше</a>
-                </div>
-            </div>
-        </div>
-        <div class="row bg_purple justify-content-center">
-            <div class="content_block col-md-10 col-xs-12">
-                <p id="why_us">Чому ми?</p>
-                <div class="container-fluid">
-                    <div class="row align-items-start">
-                        <div class="col-md-4 col-xs-12 d-flex justify-content-center flex-column">
-                            <img src="/images/about_us_img1.svg">
-                            <p class="main_text padding_top_10">
-                                Купуй своє
-                            </p>
-                            <p class="main_text font_20">
-                                Тільки українські ігри від українських розробників
-                            </p>
-                        </div>
-                        <div class="col-md-4 col-xs-12 d-flex justify-content-center flex-column">
-                            <img src="/images/about_us_img2.svg">
-                            <p class="main_text padding_top_10">
-                                Бонуси
-                            </p>
-                            <p class="main_text font_20">
-                                Отримай бонус після першої покупки 
-                            </p>
-                        </div>
-                        <div class="col-md-4 col-xs-12 d-flex justify-content-center flex-column">
-                            <img src="/images/about_us_img3.svg">
-                            <p class="main_text padding_top_10">
-                                Офіційні ключі
-                            </p>
-                            <p class="main_text font_20">
-                                Всі ключі закуповуються у офіційних дилерів
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="row bg_black justify-content-center">
             <div class="content_block col-md-10 col-xs-12">
-                <p class="main_text text_purple padding_bottom_20">Бестселери</p>
+                <p class="main_text text_purple text_bold">КАТАЛОГ</p>
+                <div class="col-md-12 col-xs-12 d-flex justify-content-end padding_bottom_20">
+                    <form class="col-md-3 col-xs-12 form_search" action="">
+                        <button class="button_search" type="submit">
+                            <img src="/images/white_search.svg">
+                        </button>
+                        <input type="text" placeholder="пошук...">
+                    </form> 
+                </div>
                 <div class="container-fluid">
                     <div class="row align-items-start">
                         <div class="col-md-3 col-xs-12 d-flex justify-content-center flex-column">
@@ -102,12 +54,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="d-flex justify-content-center padding_bottom_30">
-                    <a class="button_main button_filled font_20" href="">Більше</a>
-                </div>
 
 
-                <p class="main_text text_purple padding_bottom_20">Онлайн ігри</p>
                 <div class="container-fluid">
                     <div class="row align-items-start">
                         <div class="col-md-3 col-xs-12 d-flex justify-content-center flex-column">
@@ -148,12 +96,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="d-flex justify-content-center padding_bottom_30">
-                    <a class="button_main button_filled font_20" href="">Більше</a>
-                </div>
 
 
-                <p class="main_text text_purple padding_bottom_20">Скоро</p>
                 <div class="container-fluid">
                     <div class="row align-items-start">
                         <div class="col-md-3 col-xs-12 d-flex justify-content-center flex-column">
@@ -194,88 +138,47 @@
                         </div>
                     </div>
                 </div>
-                <div class="d-flex justify-content-center">
-                    <a class="button_main button_filled font_20" href="">Більше</a>
-                </div>
-            </div>
-        </div>
-        <div class="row bg_purple justify-content-center">
-            <div class="content_block col-md-10 col-xs-12">
-                <p class="text_yellow main_text font_30">
-                    ПІДПИСУЙСЯ НА НАШІ НОВИНИ ТА БУДЬ У КУРСІ СПЕЦІАЛЬНИХ ПРОПОЗИЦІЙ
-                </p>
-                <div class="d-flex justify-content-center padding_bottom_20">
-                    <a class="button_main button_mail text_yellow" href="">Електронна пошта...</a>
-                </div>
-                <div class="d-flex justify-content-center">
-                    <a class="button_main button_register text_yellow" href="">Продовжити</a>
-                </div>
-            </div>
-        </div>
 
-
-        <!-- footer-->
-        <div class="row bg_dark justify-content-center">
-            <div class="content_block col-md-10 col-xs-12">
                 <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-4 col-xs-12 d-flex justify-content-start flex-column align-items-start">
-                            <div class="d-flex flex-column align-items-start">
-                                <a class="game_text" href="">
-                                    Головна
-                                </a>
-                                <a class="game_text" href="">
-                                    Каталог
-                                </a>
-                                <a class="game_text" href="">
-                                    Про нас
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-4 col-xs-12 d-flex justify-content-start flex-column align-items-center">
-                            <p class="logo2">
-                                Gutsul Games
+                    <div class="row align-items-start">
+                        <div class="col-md-3 col-xs-12 d-flex justify-content-center flex-column">
+                            <img class="padding_bottom_20 full_width" src="/images/games/14.png">
+                            <p class="game_text">
+                                The Cycle
                             </p>
-                            <div class="width_200px d-flex justify-content-around">
-                                <a href="">
-                                    <img class="icon_footer" src="/images/instagram.svg">
-                                </a>
-                                <a href="">
-                                    <img class="icon_footer" src="/images/telegram.svg">
-                                </a>
-                                <a href="">
-                                    <img class="icon_footer" src="/images/facebook.svg">
-                                </a>
-                            </div>
+                            <p class="game_text padding_bottom_20">
+                                100 ₴
+                            </p>
                         </div>
-                        <div class="col-md-4 col-xs-12 d-flex justify-content-end flex-column align-items-end">
-                            <div class="d-flex flex-column align-items-start">
-                                <a class="game_text" href="">
-                                    Реєстрація
-                                </a>
-                                <a class="game_text" href="">
-                                    Логін
-                                </a>
-                                <a class="game_text" href="">
-                                    Кошик
-                                </a>    
-                            </div>
+                        <div class="col-md-3 col-xs-12 d-flex justify-content-center flex-column">
+                            <img class="padding_bottom_20 full_width" src="/images/games/14.png">
+                            <p class="game_text">
+                                Before We Leave
+                            </p>
+                            <p class="game_text padding_bottom_20">
+                                600 ₴
+                            </p>
+                        </div>
+                        <div class="col-md-3 col-xs-12 d-flex justify-content-center flex-column">
+                            <img class="padding_bottom_20 full_width" src="/images/games/14.png">
+                            <p class="game_text">
+                                LevelHead
+                            </p>
+                            <p class="game_text padding_bottom_20">
+                                250 ₴
+                            </p>
+                        </div>
+                        <div class="col-md-3 col-xs-12 d-flex justify-content-center flex-column">
+                            <img class="padding_bottom_20 full_width" src="/images/games/14.png">
+                            <p class="game_text">
+                                Saints Row
+                            </p>
+                            <p class="game_text padding_bottom_20">
+                                600 ₴
+                            </p>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="row bg_black justify-content-center">
-            <div class="content_block col-md-10 col-xs-12 d-flex flex-column">
-                <p class="main_text font_25">
-                    © Copyright 2020 Gutsul Games
-                </p>
-                <a class="main_text font_17 no_margin" href="">
-                    Правила повернення магазину
-                </a>
-                <a class="main_text font_17 no_margin" href="">
-                    Політика конфіденційності
-                </a>
             </div>
         </div>
     </div>
