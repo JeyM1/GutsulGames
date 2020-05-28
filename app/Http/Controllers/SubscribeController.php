05f11711be2a2a5f4c544f267f357edb516bad1f
JeyM1 <denafen2000@gmail.com>
2020-05-28 17:34:15 +0300

    + subscribe WIP

---
+++ app/Http/Controllers/SubscribeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SubscribeController extends Controller
+{
+    public function subscribe($email) {
+        
+    }
+
+    public function unsubscribe($email) {
+        
+    }
+}
