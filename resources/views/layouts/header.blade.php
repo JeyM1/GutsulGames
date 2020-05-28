62c3f9ed042e9dbb19f3ffff635dfd42d298cab2
JeyM1 <denafen2000@gmail.com>
2020-05-28 17:37:18 +0300

    header link to AP

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -109,6 +109,11 @@
                         <a class="dropdown-item" href="{{ route('users', Auth::user()->id) }}">
                             Мій профіль
                         </a>
+                        @if(Auth::user()->hasAdminRights())
+                        <a class="dropdown-item" href="{{ route('backpack.dashboard') }}">
+                            До адмін панелі
+                        </a>
+                        @endif
                         <a class="dropdown-item" href="{{ route('logout') }}"
                         onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();">
